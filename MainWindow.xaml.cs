6aefc0a78aa549b0407c48745a4076650549f986
eragorn08 <alexanderjames.cebreiros@gmail.com>
2022-07-23 00:33:42 +0800

    Merge Fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,61 +20,9 @@ namespace Ticketing_System
     /// </summary>
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Clear(object sender, RoutedEventArgs e)
-        {
-            uname.Text = "";
-            passw.Password = "";
-        }
-
-        private void Login(object sender, RoutedEventArgs e)
-        {
-            if(uname.Text == "123" && passw.Password == "123")
-            {
-                Window2 win2 = new Window2();
-                win2.Show();
-                this.Hide();
-            }
-            if(uname.Text == "123" && passw.Password != "123")
-            {
-                nouname.Content = "";
-            }
-            if (uname.Text != "123" && passw.Password == "123")
-            {
-                nopass.Content = "";
-            }
-            if (uname.Text == "")
-            {
-                nouname.Content = "Please Input Username";
-            }
-            else if(uname.Text != "123")
-            {
-                nouname.Content = "Wrong Username";
-
-            }
-            if (passw.Password == "")
-            {
-                nopass.Content = "Please Input Password";
-            }
-            else if(passw.Password != "123")
-            {
-                nopass.Content = "Wrong Password";
-            }
         }
     }
 }
