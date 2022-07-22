3b16e644d9764ce547e64192ececdf2f9fe54c0e
unknown <kzcdelacruz@iskolarngbayan.pup.edu.ph>
2022-07-22 20:46:41 +0800

    new push

--- Window2.xaml.cs
+++ Window2.xaml.cs
@@ -23,10 +23,5 @@ namespace Ticketing_System
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
