a2db51930010280942dfe3ea6607b53113dfc40a
eragorn08 <alexanderjames.cebreiros@gmail.com>
2022-07-24 22:40:36 +0800

    Finished with Login and Register

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Ticketing_System
 {
@@ -20,7 +22,13 @@ namespace Ticketing_System
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        string server = "localhost";
+        string database = "ticketingsystemdb";
+        string username = "root";
+        string password = "Eragorn110800";
+
+        string name, pass;
+ 
         public MainWindow()
         {
             InitializeComponent();
@@ -44,37 +52,66 @@ namespace Ticketing_System
 
         private void Login(object sender, RoutedEventArgs e)
         {
-            if(uname.Text == "123" && passw.Password == "123")
-            {
-                Window2 win2 = new Window2();
-                win2.Show();
-                this.Hide();
-            }
-            if(uname.Text == "123" && passw.Password != "123")
-            {
-                nouname.Content = "";
-            }
-            if (uname.Text != "123" && passw.Password == "123")
-            {
-                nopass.Content = "";
-            }
-            if (uname.Text == "")
-            {
-                nouname.Content = "Please Input Username";
-            }
-            else if(uname.Text != "123")
-            {
-                nouname.Content = "Wrong Username";
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection conn = new MySqlConnection(constring);
 
-            }
-            if (passw.Password == "")
+            int i = 0;
+            int log_perm = 0;
+            name = uname.Text;
+            pass = passw.Password;
+            MySqlDataReader perm;
+
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from empuser where Username=@name and Password=@pass";
+            cmd.Parameters.Add("@name", MySqlDbType.String).Value = name;
+            cmd.Parameters.Add("@pass", MySqlDbType.String).Value = pass;
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            perm = cmd.ExecuteReader();
+
+            while (perm.Read())
             {
-                nopass.Content = "Please Input Password";
+                log_perm = perm.GetInt32("Permission");
             }
-            else if(passw.Password != "123")
+
+            if (i == 0)
             {
-                nopass.Content = "Wrong Password";
+                MessageBoxResult msgSure = MessageBox.Show("No Account Associated with Inputted Username and Password!", "Confirmation");
             }
+            else
+            {
+                if(log_perm == 1)
+                {
+                    Window4 win4 = new Window4();
+                    win4.Show();
+                    this.Hide();
+                }
+                if(log_perm == 2)
+                {
+                    Window2 win2 = new Window2();
+                    win2.Show();
+                    this.Hide();
+                }
+                if(log_perm == 3)
+                {
+                    Window3 win3 = new Window3();
+                    win3.Show();
+                    this.Hide();
+                }
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            win1.Show();
+            this.Hide();
         }
     }
 }
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -11,17 +11,115 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Ticketing_System
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
+        //SQL Status
+        string server = "localhost";
+        string database = "ticketingsystemdb";
+        string username = "root";
+        string password = "Eragorn110800";
+
+        string name, uname, pass, cpass;
         public Window1()
         {
             InitializeComponent();
+
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection conn = new MySqlConnection(constring);
+
         }
+
+        private void _return_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainwin = new MainWindow();
+            mainwin.Show();
+            this.Hide();
+        }
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            rName.Text = "";
+            rPass.Text = "";
+            rUName.Text = "";
+            rCPass.Text = "";
+        }
+        private void reg_Click(object sender, RoutedEventArgs e)
+        {
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection conn = new MySqlConnection(constring);
+
+            name = rName.Text;
+            uname = rUName.Text;
+            pass = rPass.Text;
+            cpass = rCPass.Text;
+
+            if (name == "")
+            {
+                nname.Content = "Please Input Name";
+            } 
+            if (uname == "")
+            {
+                nuname.Content = "Please Input Username";
+            }
+            if (pass == "")
+            {
+                npass.Content = "Please Input Password";
+            }
+            if (cpass == "")
+            {
+                ncpass.Content = "Please Repeat Password";
+            }
+            if(name != "" && uname != "" && pass != "" && cpass != "")
+            {
+                if (pass == cpass)
+                {
+                    string datetime = DateTime.Now.ToString("yyyy-MM-dd");
+
+                    MySqlCommand cmd = new MySqlCommand("INSERT INTO empuser(Name,Username,Password,Permission,AddedOn) Values(@name,@uname,@pass,3,@datetime);", conn);
+                    cmd.Parameters.Add("@name", MySqlDbType.String).Value = name;
+                    cmd.Parameters.Add("@uname", MySqlDbType.String).Value = uname;
+                    cmd.Parameters.Add("@pass", MySqlDbType.String).Value = pass;
+                    cmd.Parameters.Add("@datetime", MySqlDbType.String).Value = datetime;
+                    conn.Open();
+                    MySqlDataReader read_gen = cmd.ExecuteReader();
+
+                    MessageBoxResult msgSure = MessageBox.Show("User Registered!", "Confirmation");
+                    rName.Text = "";
+                    rPass.Text = "";
+                    rUName.Text = "";
+                    rCPass.Text = "";
+                }
+                else
+                {
+                    MessageBoxResult msgSure = MessageBox.Show("Password is not the same with Confirm Password", "Confirmation");
+
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rUName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rPass_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rCPass_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
     }
 }
